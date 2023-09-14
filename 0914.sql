1eb4e8237d7e3e0076140800483b443b7b73d130
TommyLee <kokokorkr@naver.com>
2023-09-14 12:38:54 +0900

    0914 / ORDER BY

--- 0914.sql
+++ 0914.sql
@@ -19,26 +19,44 @@ FROM EMP;
 -- 실습문제 --
 -- 1. 사원이름이 S로 끝나는 사원 데이터 출력
 SELECT * FROM EMP
-WHERE ENAME LIKE '%S'
+WHERE ENAME LIKE '%S';
 
 -- 2. 30번 부서에서 근무하는 사원 중 직책이 SALESMAN인 사원의 
 -- 사원번호, 이름, 직책, 급여, 부서번호 출력
-SELECT EMPNO, ENAME, JOB, DEPTNO 
+SELECT EMPNO, ENAME, JOB, SAL, DEPTNO 
 FROM EMP
-
+WHERE DEPTNO = 30 
+AND JOB IN 'SALESMAN';
 
 -- 3, 20번, 30번 부서에 근무하는 사원 중 급여가 2000 초과인 사원의
 -- 사원번호, 이름, 급여, 부서번호 출력
-
+SELECT EMPNO, ENAME, SAL, DEPTNO 
+FROM EMP
+WHERE DEPTNO IN (20, 30) AND SAL > 2000;
 
 -- 4. BETWEEN 연산자 사용을 하지 않고 급여가 2000 이상 3000이하 데이터 출력
-
+SELECT * 
+FROM EMP
+WHERE SAL >=2000 AND SAL <= 3000;
 
 -- 5. 사원 이름에 e가 포함되어 있는 30번 부서의 사원 중 급여가 1000~2000 사이가 아닌 사원의
 -- 이름, 사원번호, 급여, 부서번호 출력
+SELECT EMPNO, ENAME, SAL, DEPTNO 
+FROM EMP
+WHERE DEPTNO = 30
+AND ENAME LIKE '%E%'
+AND SAL NOT BETWEEN 1000 AND 2000;
+
+SELECT EMPNO, ENAME, SAL, DEPTNO 
+FROM EMP
+WHERE ENAME LIKE '%E%';
 
 
--- 6. 추가 수당이 존재하지 않고 상급자의 직책이 MANAGER, CLERK인 사원 중
+-- 6. 추가 수당이 존재하지 않고 상급자가 있고, 직책이 MANAGER, CLERK인 사원 중
 -- 사원 이름의 두 번째 글자가 L이 아닌 사원의 정보를 출력
+SELECT * FROM EMP
+WHERE COMM IS NULL 
+AND MGR IS NOT NULL
+AND JOB IN('MAMAGER', 'CLERK')
+AND ENAME NOT LIKE '_L%';
 
-GFCXGGG
